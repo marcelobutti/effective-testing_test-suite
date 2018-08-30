ba1f63050aad3bccff1241ab0596af5d92c6890b
MarceloB <marcelobutti@hotmail.com>
2018-08-30 10:12:05 -0300

    ejercicio listo

--- src/main/java/com/effectivetesting/examples/Car.java
+++ src/main/java/com/effectivetesting/examples/Car.java
@@ -3,5 +3,6 @@ package com.effectivetesting.examples;
 public class Car {
 	public int wheels = 4;
 	public String type = "Sedan";
+	public String segment = "A";
 	public boolean isDiesel = false;
 }
--- src/main/java/com/effectivetesting/examples/CarDriver.java
+++ src/main/java/com/effectivetesting/examples/CarDriver.java
@@ -3,6 +3,52 @@ package com.effectivetesting.examples;
 public class CarDriver {
 	public static void main(String[] args) {
 		Car myCar = new Car();
+		Car myC2 = new Car();
+		myC2.isDiesel=true;
+		myC2.segment="Suv";
 		System.out.println(myCar.type);
+		System.out.println(myCar.wheels + myCar.type);
+		
+		if (myCar.isDiesel == true) {
+			System.out.println("Use Infinia Diesel");
+		} else {
+			System.out.println("Use Infinia Nafta");
+		}
+		
+		if (myC2.isDiesel == true) {
+			System.out.println("Use Infinia Diesel");
+		} else {
+			System.out.println("Use Infinia Nafta");
+		}
+		
+		
+		
+		switch (myCar.segment) {
+		case "A":
+			System.out.println("City car");
+			break;
+		case "B":
+		System.out.println("Compact");
+			break;
+		case "Suv":
+		System.out.println("Sport Utility Vehicle");
+		break;
+		default:
+		}
+		
+		switch (myC2.segment) {
+		case "A":
+			System.out.println("City car");
+			break;
+		case "B":
+		System.out.println("Compact");
+			break;
+		case "Suv":
+		System.out.println("Sport Utility Vehicle");
+		break;
+		default:
+		}
+		
+		
 	}
 }
